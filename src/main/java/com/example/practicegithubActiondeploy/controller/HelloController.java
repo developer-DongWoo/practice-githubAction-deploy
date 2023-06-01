8eeab0e65fd844e2bc565e222cb63147326f8ddb
developer-DongWoo <ehddn8547@gmail.com>
2023-06-01 15:08:16 +0900

    14th

--- src/main/java/com/example/practicegithubActiondeploy/controller/HelloController.java
+++ src/main/java/com/example/practicegithubActiondeploy/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+        return "hello!";
     }
 }
